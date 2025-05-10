39535855bdea21e8f9df7a4aacbe044148a0985a
Emanuel Costa <emanuel.robert@gmail.com>
2025-05-09 23:50:39 -0300

    atualizando o site

--- galeria/models.py
+++ galeria/models.py
@@ -1,10 +1,22 @@
 from django.db import models
+from datetime import datetime
 
 class Fotografia(models.Model):
+    
+    opcoes_categoria = [
+        ('Nebulosa', 'nebulosa'),
+        ('Estrela', 'estrela'),
+        ('Galáxia', 'galaxia'),
+        ('Planeta', 'planeta'),
+    ]
+    
     nome = models.CharField(max_length=100, null=False, blank=False)
     legenda = models.CharField(max_length=200, null=False, blank=False)
+    categoria = models.CharField(choices=opcoes_categoria, default='')
     descricao = models.TextField(null=False, blank=False)
-    foto = models.CharField(max_length=100, null=False, blank=False)
+    foto = models.ImageField(upload_to='fotos/%Y/%m/%d', blank=True)
+    publicada = models.BooleanField(default=False)
+    data_publicacao = models.DateField(default=datetime.now, blank=False)
     
     def __str__(self):
-        return f'Fotografia [nome={self.nome}]'
+        return self.nome
--- setup/settings.py
+++ setup/settings.py
@@ -118,6 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
+
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
@@ -125,6 +126,12 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# Media
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
